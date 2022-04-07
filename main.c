5e22718bdc050631fcbc46dd932a1100539da26f
Alina Makhaeva <lina.makhaeva@gmail.com>
2022-04-08 00:24:44 +0700

    second commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int dlina, shirina, ploshad;
+    printf("Dlina and shirina ");
+    scanf("%d %d", &dlina, &shirina);
+    ploshad = dlina * shirina;
+    printf("%d\n", ploshad);
+    return 0;
+}
